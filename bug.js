3abb7a0cd02967ba624492309b14b820197f417a
BugHunter-X <askzenai@gmail.com>
2025-01-20 02:20:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling if userId is not a number
+  const user = users.find(user => user.id === parseInt(userId));
+  if (!user) {
+    // Missing a proper 404 response
+    res.send('User not found');
+  }
+  res.json(user);
+});
